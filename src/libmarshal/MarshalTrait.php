16b18a89539cd98652cba045335be4c0784ef36c
Matthew Jordan <mattjordanbusiness@gmail.com>
2022-04-10 03:28:28 -0500

    Implement caching and overall better type checking

--- src/libmarshal/MarshalTrait.php
+++ src/libmarshal/MarshalTrait.php
@@ -7,10 +7,11 @@ namespace libMarshal;
 use libMarshal\attributes\Field;
 use libMarshal\exception\GeneralMarshalException;
 use libMarshal\exception\UnmarshalException;
-use libMarshal\exception\MarshalException;
 use ReflectionClass;
+use ReflectionException;
+use ReflectionNamedType;
 use ReflectionProperty;
-use RuntimeException;
+use ReflectionUnionType;
 
 /**
  * This is the main trait used for marshaling/demarshaling data.
@@ -30,24 +31,41 @@ use RuntimeException;
  * }
  */
 trait MarshalTrait {
+	/**
+	 * A ReflectionClass instance used to fetch properties, values, etc.
+	 * @var ReflectionClass<static>
+	 */
+	private static ReflectionClass $reflectedInstance;
+	/**
+	 * A list of {@link PropertyHolder} instances used to fetch properties, field attributes, etc.
+	 * @var array<int, PropertyHolder>
+	 */
+	private static array $cachedHolders;
 
+	/**
+	 * @throws GeneralMarshalException
+	 */
 	public function marshal(): array {
-		$data = [];
-		$reflected = new ReflectionClass($this);
-		foreach ($reflected->getProperties() as $property) {
-			$field = self::getField($property);
-			if($field === null) {
-				continue;
+		try {
+			$data = [];
+			foreach (self::getHolders() as $holder) {
+				// Get the reflection property + field attribute from the holder
+				[$property, $field] = $holder->asArray();
+
+				// Get the current value of the property
+				$value = $this->{$property->getName()};
+				// If the value is an object and has any type classes with the trait, we override
+				if(is_object($value) && count($holder->getTypeClasses()) > 0) {
+					$value = $value->marshal();
+				}
+				// Update the array with the value
+				$name = $field->name !== "" ? $field->name : $property->getName();
+				$data[$name] = $value;
 			}
-			$value = $this->{$property->getName()};
-			// If the value is an object and has the trait, we override
-			if(is_object($value) && self::hasTrait(new ReflectionClass($value), MarshalTrait::class)) {
-				$value = $value->marshal();
-			}
-			$name = strlen($field->name) > 0 ? $field->name : $property->getName();
-			$data[$name] = $value;
+			return $data;
+		} catch(ReflectionException $exception) {
+			throw new GeneralMarshalException($exception->getMessage(), $exception->getCode(), $exception);
 		}
-		return $data;
 	}
 
 	/**
@@ -56,45 +74,76 @@ trait MarshalTrait {
 	 * @return MarshalTrait
 	 * @throws GeneralMarshalException
 	 * @throws UnmarshalException
+	 * @noinspection PhpRedundantCatchClauseInspection - Given that we are using reflection to invoke the unmarshal method, we can ignore PHPStorm's warning about redundancy
 	 */
 	public static function unmarshal(array $data, bool $strict = true): static {
-		$reflected = new ReflectionClass(static::class);
-		/** @var static $instance */
-		$instance = $reflected->newInstanceWithoutConstructor();
-		foreach($reflected->getProperties() as $property) {
-			$field = self::getField($property);
-			// Property doesn't have the Field attribute, so we skip it
-			if($field === null) {
-				continue;
-			}
-			$name = strlen($field->name) > 0 ? $field->name : $property->getName();
-			$value = $data[$name] ?? null;
-			if($value === null && $strict) {
-				throw new UnmarshalException("Missing field '$name'");
-			}
-			// Ensure that the value is of the correct type
-			self::checkType($property, $value);
+		try {
+			/** @var static $instance */
+			$instance = self::getReflectedInstance()->newInstanceWithoutConstructor();
+			foreach(self::getHolders() as $holder) {
+				// Get the property and field from the holder
+				[$property, $field] = $holder->asArray();
 
-			// If the value is an array or an stdClass, we can check if it has the MarshalTrait and if so, we can unmarshal it
-			if(is_array($value) || $value instanceof \stdClass) {
-				// Get class type associated with the property
-				$type = $property->getType();
-				if($type !== null) {
-					$class = new ReflectionClass($type->getName());
-					if(self::hasTrait($class, MarshalTrait::class)) {
-						$value = $class->getMethod("unmarshal")->invoke(
-							null, $value, $strict
-						);
-					}
+				// Get the name of the property
+				$name = $field->name !== "" ? $field->name : $property->getName();
+				// Fetch the value
+				$value = $data[$name] ?? null;
+				// If the value is null & strict is true, throw an exception
+				if($value === null && $strict) {
+					throw new UnmarshalException("Missing field '$name'");
 				}
+				// If the value is an array, we can check if it has the MarshalTrait and if so, we can unmarshal it
+				if(is_array($value) && count($holder->getTypeClasses()) > 0) {
+					foreach($holder->getTypeClasses() as $type) {
+						try {
+							$method = $type->getMethod("unmarshal");
+							$value = $method->invoke($instance, $value, $strict);
+							break;
+						}
+						catch(ReflectionException|UnmarshalException $_) {
+							// We don't care about this exception, we just want to try the next type
+						}
+					}
+				} else {
+					// Ensure that the value is of the correct type
+					self::checkType($property, $value);
+				}
+				$instance->{$property->getName()} = $value;
 			}
-			$instance->{$property->getName()} = $value;
+			return $instance;
+		} catch(ReflectionException $exception) {
+			throw new GeneralMarshalException($exception->getMessage(), $exception->getCode(), $exception);
 		}
-		return $instance;
+	}
+
+	private static function getReflectedInstance(): ReflectionClass {
+		return self::$reflectedInstance ??= new ReflectionClass(static::class);
 	}
 
 	/**
-	 * @throws GeneralMarshalException
+	 * A lazy getter/initializer for getting the {@link PropertyHolder} instances for the implementing class.
+	 *
+	 * @return array<int, PropertyHolder>
+	 */
+	private static function getHolders(): array {
+		return self::$cachedHolders ??= array_map(
+			callback: static fn(ReflectionProperty $property) => new PropertyHolder(
+				property: $property,
+				field: self::getField($property)
+			),
+			array: array_filter(
+				array: self::getReflectedInstance()->getProperties(),
+				callback: fn(ReflectionProperty $property): bool => self::getField($property) !== null
+			)
+		);
+	}
+
+	/**
+	 * Attempts to get the Field attribute from a property
+	 * If the property doesn't have the Field attribute, it will return null
+	 *
+	 * @param ReflectionProperty $property
+	 * @return Field|null
 	 */
 	private static function getField(ReflectionProperty $property): ?Field {
 		$attribute = $property->getAttributes(Field::class)[0] ?? null;
@@ -102,9 +151,7 @@ trait MarshalTrait {
 			return null;
 		}
 		$field = $attribute->newInstance();
-		if(!($field instanceof Field)) {
-			throw new GeneralMarshalException("Field attribute must be an instance of Field");
-		}
+		assert($field instanceof Field, "Field attribute must be an instance of Field");
 		return $field;
 	}
 
@@ -112,10 +159,10 @@ trait MarshalTrait {
 	 * Returns true if a given class has a trait on it
 	 *
 	 * @param ReflectionClass $class
-	 * @param string $traitClass
+	 * @param class-string $traitClass
 	 * @return bool
 	 */
-	private static function hasTrait(ReflectionClass $class, string $traitClass): bool {
+	public static function hasTrait(ReflectionClass $class, string $traitClass): bool {
 		return count(array_filter($class->getTraits(), fn(ReflectionClass $trait) => $trait->getName() === $traitClass)) === 1;
 	}
 
@@ -124,14 +171,14 @@ trait MarshalTrait {
 	 */
 	private static function checkType(ReflectionProperty $property, mixed $value): void {
 		$type = $property->getType();
-		if($type === null) {
+		if($type === null || ($value === null && $type->allowsNull())) {
 			return;
 		}
 		$valueTypeName = get_debug_type($value);
 		$typeName = $type->getName();
-		if($type instanceof \ReflectionNamedType && $valueTypeName !== $typeName) {
+		if($type instanceof ReflectionNamedType && $valueTypeName !== $typeName) {
 			throw new GeneralMarshalException("Field '{$property->getName()}' must be of type '$typeName', got '$valueTypeName'");
-		} else if($type instanceof \ReflectionUnionType && !in_array($valueTypeName, $type->getTypes(), true)) {
+		} else if($type instanceof ReflectionUnionType && !in_array($valueTypeName, $type->getTypes(), true)) {
 			throw new GeneralMarshalException("Field '{$property->getName()}' must be of type '$typeName', got '$valueTypeName'");
 		}
 		// TODO: 8.1 now supports ReflectionIntersectionType, so when that is more widely used, we can add support for it here
