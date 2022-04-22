50b831d8c035b5b9a1055ae6ac98294c98b9d701
Em Jordan <sylvrs@proton.me>
2022-04-22 02:14:56 -0500

    Update tests

--- tests/libMarshal/UnmarshalTest.php
+++ tests/libMarshal/UnmarshalTest.php
@@ -9,12 +9,12 @@ use PHPUnit\Framework\TestCase;
 final class UnmarshalTest extends TestCase {
 
 	public function testUnmarshalUser(): void {
-		$user = new User("John", "Doe", 42, "test@gmail.com");
+		$user = new User("John", "Doe", 42, ["testing@gmail.com"], "test@gmail.com");
 		$this->assertEquals(User::unmarshal($user->marshal(), false), $user);
 	}
 
 	public function testUnmarshalEmbeddedUserWithNullOptions(): void {
-		$user = new EmbeddedUser(firstName: "John", lastName: "Doe", age: 42, email: "johndoe@gmail.com");
+		$user = new EmbeddedUser(firstName: "John", lastName: "Doe", age: 42, contacts: ["janedoe@gmail.com"], email: "johndoe@gmail.com");
 		$this->assertEquals(EmbeddedUser::unmarshal($user->marshal(), false), $user);
 	}
 
