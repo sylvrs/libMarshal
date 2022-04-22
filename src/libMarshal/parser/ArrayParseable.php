2c9a730d48bdb2731d87e80022c83fff5542f99a
Matthew Jordan <mattjordanbusiness@gmail.com>
2022-04-22 04:46:18 -0500

    ArrayParseable: Add generic templates for key and values

--- src/libMarshal/parser/ArrayParseable.php
+++ src/libMarshal/parser/ArrayParseable.php
@@ -6,19 +6,23 @@ namespace libMarshal\parser;
 
 /**
  * @template U of mixed
+ *
+ * @template K of mixed
+ * @template V of mixed
+ *
  * @extends Parseable<array, U>
  */
 interface ArrayParseable extends Parseable {
 
 	/**
-	 * @param array $data
+	 * @param array<K,V> $data
 	 * @return U
 	 */
 	public function parse(mixed $data): mixed;
 
 	/**
 	 * @param U $data
-	 * @return array
+	 * @return array<K,V>
 	 */
 	public function serialize(mixed $data): array;
 }
