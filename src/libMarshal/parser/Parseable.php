dc5ba0c83bdc6e8ec74033f161d3f083a4160eb4
Matthew Jordan <mattjordanbusiness@gmail.com>
2022-04-22 04:49:07 -0500

    Parseable: Add documentation about the interface

--- src/libMarshal/parser/Parseable.php
+++ src/libMarshal/parser/Parseable.php
@@ -5,6 +5,14 @@ declare(strict_types=1);
 namespace libMarshal\parser;
 
 /**
+ * The `Parseable` interface is used when defining custom parsing of a field.
+ * To specify custom parsing, create a class that implements this interface and
+ * pass the class string to the `Field` constructor using the `parser` property.
+ *
+ * As an example:
+ * #[Field(name: "Test", parser: TestParser::class)]
+ * protected TestClass $test;
+ *
  * @template T of mixed
  * @template U of mixed
  */
