ce9cdcdbd3ddfe7a1fffcec0f97a27ddb18bcd46
Em Jordan <sylvrs@proton.me>
2022-04-23 01:16:24 -0500

    Update tests

--- tests/libMarshal/User.php
+++ tests/libMarshal/User.php
@@ -13,20 +13,17 @@ class User {
 	 * @param string $firstName
 	 * @param string $lastName
 	 * @param int $age
+	 * @param float $height
 	 * @param string[] $contacts
 	 * @param string|null $email
 	 */
 	public function __construct(
-		#[Field(name: "first-name")]
-		public string $firstName,
-		#[Field(name: "last-name")]
-		public string $lastName,
-		#[Field]
-		public int $age,
-		#[Field]
-		public array $contacts = [],
-		#[Field]
-		public ?string $email = null,
+		#[Field(name: "first-name")] public string $firstName,
+		#[Field(name: "last-name")] public string $lastName,
+		#[Field] public int $age,
+		#[Field] public float $height,
+		#[Field] public array $contacts = [],
+		#[Field] public ?string $email = null,
 	)
 	{
 	}
