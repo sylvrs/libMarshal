2d19e054cb1d07515f0c3e766f85269afadf6a72
Em Jordan <sylvrs@proton.me>
2022-04-23 10:07:26 -0500

    Update tests to check primitive arrays

--- tests/libMarshal/UnmarshalTest.php
+++ tests/libMarshal/UnmarshalTest.php
@@ -9,18 +9,18 @@ use PHPUnit\Framework\TestCase;
 final class UnmarshalTest extends TestCase {
 
 	public function testUnmarshalUser(): void {
-		$user = new User(firstName: "John", lastName: "Doe", age: 42, height: 1.78, contacts: ["testing@gmail.com"], email: "test@gmail.com");
+		$user = new User(firstName: "John", lastName: "Doe", age: 42, height: 1.78, contacts: ["janedoe@gmail.com", "jimdoe@gmail.com"], email: "test@gmail.com");
 		$this->assertEquals(User::unmarshal($user->marshal(), false), $user);
 	}
 
 	public function testUnmarshalEmbeddedUser(): void {
 		$options = new Options(name: "Test", type: "Embedded Options", testField: 456);
-		$user = new EmbeddedUser(firstName: "John", lastName: "Doe", age: 42, height: 1.78, contacts: ["janedoe@gmail.com"], email: "johndoe@gmail.com", options: $options);
+		$user = new EmbeddedUser(firstName: "John", lastName: "Doe", age: 42, height: 1.78, contacts: ["janedoe@gmail.com", "jimdoe@gmail.com"], email: "johndoe@gmail.com", options: $options);
 		$this->assertEquals(EmbeddedUser::unmarshal($user->marshal()), $user);
 	}
 
 	public function testUnmarshalEmbeddedUserWithNullOptions(): void {
-		$user = new EmbeddedUser(firstName: "John", lastName: "Doe", age: 42, height: 1.78, contacts: ["janedoe@gmail.com"], email: "johndoe@gmail.com");
+		$user = new EmbeddedUser(firstName: "John", lastName: "Doe", age: 42, height: 1.78, contacts: ["janedoe@gmail.com", "jimdoe@gmail.com"], email: "johndoe@gmail.com");
 		$this->assertEquals(EmbeddedUser::unmarshal($user->marshal()), $user);
 	}
 
