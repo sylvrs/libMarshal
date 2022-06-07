bae5ba145f485a49bc53b6a44cc290d9962196fa
커피와 우유 <106326028+keopiwauyu@users.noreply.github.com>
2022-06-07 08:46:26 +0800

    Renamer attribute

---
+++ src/libMarshal/attributes/Renamer.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace libMarshal\attributes;
+
+use Closure;
+use Attribute;
+use libMarshal\parser\Parseable;
+
+#[Attribute(Attribute::TARGET_CLASS)]
+class Renamer {
+    
+    /**
+     * @var Closure(string): string
+     */
+    protected Closure $renamer;
+
+	/**
+	 * @param callable(string): string Renamer function applies on all fields in a class.
+	 */
+	public function __construct(
+        callable $renamer
+	)
+	{
+		$this->renamer = Closure::fromCallable($renamer);
+	}
+
+    /**
+     * @return callable(string): string
+     */
+    public function getRenamer(): callable {
+        return $this->renamer;
+    }
+}
