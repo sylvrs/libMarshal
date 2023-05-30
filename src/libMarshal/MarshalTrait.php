18c84d962552613da43ec88144bc410eeab3e9cc
Matthew Jordan <mattjordanbusiness@gmail.com>
2023-05-30 17:53:25 -0500

    chore!(trait): resolve phpstan issues & simplify error handling by removing classes

--- src/libMarshal/MarshalTrait.php
+++ src/libMarshal/MarshalTrait.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace libMarshal;
 
+use AssertionError;
 use libMarshal\attributes\Field;
 use libMarshal\exception\FileNotFoundException;
-use libMarshal\exception\GeneralMarshalException;
 use libMarshal\exception\UnmarshalException;
 use libMarshal\parser\Parseable;
 use ReflectionAttribute;
@@ -29,6 +29,7 @@ use function is_array;
 use function is_object;
 use function json_decode;
 use function json_encode;
+use function method_exists;
 use function yaml_emit_file;
 use function yaml_parse_file;
 
@@ -61,90 +62,86 @@ trait MarshalTrait {
 	private static array $cachedHolders;
 
 	/**
-	 * @throws GeneralMarshalException
+	 * @return array<string, mixed>
 	 */
 	public function marshal(): array {
-		try {
-			$data = [];
-			foreach (self::getHolders() as $holder) {
-				// Get the reflection property + field attribute from the holder
-				[$property, $field] = $holder->asArray();
-				// Get the current value of the property
-				$value = $this->{$property->getName()};
-				$parser = $holder->getParser();
-				// Update the array with the value
-				$name = $field->name !== "" ? $field->name : $property->getName();
-				$data[$name] = match (true) {
-					// If the value is an object and has any type classes with the trait, we override
-					is_object($value) && count($holder->getTypeClasses()) > 0 => $value->marshal(),
-					// If the holder has an associated parser, use that for marshaling
-					$parser instanceof Parseable => $parser->serialize($value),
-					// Otherwise, use the default value
-					default => $value
-				};
-			}
-			return $data;
-		} catch (ReflectionException $exception) {
-			throw new GeneralMarshalException($exception->getMessage(), $exception->getCode(), $exception);
+		$data = [];
+		foreach (self::getHolders() as $holder) {
+			// Get the reflection property + field attribute from the holder
+			[$property, $field] = $holder->asArray();
+			// Get the current value of the property
+			$value = $this->{$property->getName()};
+			$parser = $holder->getParser();
+			// Update the array with the value
+			$name = $field->name !== "" ? $field->name : $property->getName();
+			$data[$name] = match (true) {
+				// If the value is an object and has any type classes with the trait, we override
+				is_object($value) && method_exists($value, "marshal") && count($holder->getTypeClasses()) > 0 => $value->marshal(),
+				// If the holder has an associated parser, use that for marshaling
+				$parser instanceof Parseable => $parser->serialize($value),
+				// Otherwise, use the default value
+				default => $value
+			};
 		}
+		return $data;
 	}
 
 	/**
-	 * @param array $data - The data to unmarshal with
+	 * @param array<string, mixed> $data - The data to unmarshal with
 	 * @param bool $strict - Whether to throw an exception if a field is missing
-	 * @return MarshalTrait
-	 * @throws GeneralMarshalException
+	 *
 	 * @throws UnmarshalException
-	 * @noinspection PhpRedundantCatchClauseInspection - Given that we are using reflection to invoke the unmarshal method, we can ignore PHPStorm's warning about redundancy
 	 */
 	public static function unmarshal(array $data, bool $strict = true): static {
-		try {
-			// Create a new instance of the class
-			/** @var static $instance */
-			$instance = self::getReflectedInstance()->newInstanceWithoutConstructor();
-			foreach (self::getHolders() as $holder) {
-				// Get the property and field from the holder
-				[$property, $field] = $holder->asArray();
+		// Create a new instance of the class
+		/** @var static $instance */
+		$instance = self::getReflectedInstance()->newInstanceWithoutConstructor();
+		foreach (self::getHolders() as $holder) {
+			// Get the property and field from the holder
+			[$property, $field] = $holder->asArray();
 
-				// Get the name of the property
-				$name = $field->name !== "" ? $field->name : $property->getName();
-				// Fetch the value
-				$value = $data[$name] ?? $property->getDefaultValue();
-				// If the value is null, doesn't allow null, & strict is true, throw an exception
-				if ($value === null && !$holder->allowsNull() && $strict) {
-					throw new UnmarshalException("Missing field '$name'");
-				}
-				if (($parser = $holder->getParser()) instanceof Parseable) {
-					// If the holder has an associated parser, use that for unmarshaling
-					$value = $parser->parse($value);
-				} else if (is_array($value) && count($holder->getTypeClasses()) > 0) {
-					// If the value is an array, we can check if it has the MarshalTrait and if so, we can unmarshal it
-					foreach ($holder->getTypeClasses() as $type) {
-						try {
-							$method = $type->getMethod(__FUNCTION__);
-							$value = $method->invoke($instance, $value, $strict);
-							break;
-						} catch (ReflectionException|GeneralMarshalException) {
-							// We don't care about this exception, we just want to try the next type
-						}
-					}
-				} else {
-					// Ensure that the value is of the correct type
-					self::checkType($property, $value);
-				}
-				// Set the value on the instance
-				$instance->{$property->getName()} = $value;
+			// Get the name of the property
+			$name = $field->name !== "" ? $field->name : $property->getName();
+
+			// Do not set the field if the user allows it to be uninitialized
+			if (!isset($data[$name]) && !$property->hasDefaultValue() && $field->allowUninitialized) {
+				continue;
 			}
-			return $instance;
-		} catch (ReflectionException $exception) {
-			throw new GeneralMarshalException($exception->getMessage(), $exception->getCode(), $exception);
+
+			// Fetch the value
+			$value = $data[$name] ?? $property->getDefaultValue();
+			// If the value is null, doesn't allow null, & strict is true, throw an exception
+			if ($value === null && !$holder->allowsNull() && $strict) {
+				throw new UnmarshalException("Missing field '$name'");
+			}
+			if (($parser = $holder->getParser()) instanceof Parseable) {
+				// If the holder has an associated parser, use that for unmarshaling
+				$value = $parser->parse($value);
+			} else if (is_array($value) && count($holder->getTypeClasses()) > 0) {
+				// If the value is an array, we can check if it has the MarshalTrait and if so, we can unmarshal it
+				foreach ($holder->getTypeClasses() as $type) {
+					try {
+						$method = $type->getMethod(__FUNCTION__);
+						$value = $method->invoke($instance, $value, $strict);
+						break;
+					} catch (ReflectionException|UnmarshalException) {
+						// We don't care about this exception, we just want to try the next type
+					}
+				}
+			} else {
+				// Ensure that the value is of the correct type
+				self::checkType($property, $value);
+			}
+			// Set the value on the instance
+			$instance->{$property->getName()} = $value;
 		}
+		return $instance;
 	}
 
 	/**
 	 * This method is used to marshal the object and save it into a YAML file
 	 *
-	 * @throws GeneralMarshalException
+	 * @param array<class-string, callable> $callbacks
 	 */
 	public function saveToYaml(string $fileName, int $encoding = YAML_ANY_ENCODING, int $linebreak = YAML_ANY_BREAK, array $callbacks = []): bool {
 		return yaml_emit_file(
@@ -152,6 +149,7 @@ trait MarshalTrait {
 			data: $this->marshal(),
 			encoding: $encoding,
 			linebreak: $linebreak,
+			/** @phpstan-ignore-next-line - stubs for this are wrong & don't include the callbacks param */
 			callbacks: $callbacks
 		);
 	}
@@ -159,63 +157,62 @@ trait MarshalTrait {
 	/**
 	 * This method is used to load YAML data from a file and unmarshal it into an instance of the trait user
 	 *
-	 * @throws GeneralMarshalException
+	 * @param array<class-string, callable> $callbacks
 	 * @throws UnmarshalException
 	 */
 	public static function loadFromYaml(string $fileName, bool $strict = true, int $pos = 0, ?int &$ndocs = null, array $callbacks = []): static {
 		if (!file_exists($fileName)) {
 			throw new FileNotFoundException("The file '$fileName' does not exist");
 		}
-		return self::unmarshal(
-			data: yaml_parse_file(
-				filename: $fileName,
-				pos: $pos,
-				ndocs: $ndocs,
-				callbacks: $callbacks
-			),
-			strict: $strict
-		);
+
+		// parse and verify the data
+		/** @phpstan-ignore-next-line - stubs for this are wrong & don't include the callbacks param */
+		$data = yaml_parse_file(filename: $fileName, pos: $pos, ndocs: $ndocs, callbacks: $callbacks);
+		if (!is_array($data)) {
+			throw new UnmarshalException("Data loaded from file '$fileName' is not a valid object");
+		}
+		return self::unmarshal(data: $data, strict: $strict);
 	}
 
 	/**
 	 * This method is used to marshal the object and save it into a JSON file
 	 *
-	 * @throws GeneralMarshalException
+	 * @param int<1, max> $depth
 	 */
 	public function saveToJson(string $fileName, int $flags = 0, int $depth = 512): int|false {
 		return file_put_contents(
 			filename: $fileName,
-			data: json_encode(
-				value: $this->marshal(),
-				flags: $flags,
-				depth: $depth
-			)
+			data: json_encode(value: $this->marshal(), flags: $flags, depth: $depth)
 		);
 	}
 
 	/**
 	 * This method is used to load JSON data from a file and unmarshal it into an instance of the trait user
 	 *
-	 * @throws GeneralMarshalException
+	 * @param int<1, max> $depth
 	 * @throws UnmarshalException
 	 */
 	public static function loadFromJson(string $fileName, bool $strict = true, int $depth = 512, int $flags = 0): static {
 		if (!file_exists($fileName)) {
 			throw new FileNotFoundException("The file '$fileName' does not exist");
 		}
-		return self::unmarshal(
-			data: json_decode(
-				json: file_get_contents(
-					filename: $fileName
-				),
-				associative: true,
-				depth: $depth,
-				flags: $flags
-			),
-			strict: $strict
-		);
+
+		$raw = file_get_contents(filename: $fileName);
+		if ($raw === false) {
+			throw new UnmarshalException("Failed to read file '$fileName'");
+		}
+
+		$data = json_decode(json: $raw, associative: true, depth: $depth, flags: $flags);
+		if (!is_array($data)) {
+			throw new UnmarshalException("The file '$fileName' does not contain a valid JSON object");
+		}
+
+		return self::unmarshal(data: $data, strict: $strict);
 	}
 
+	/**
+	 * @return ReflectionClass<static>
+	 */
 	private static function getReflectedInstance(): ReflectionClass {
 		return self::$reflectedInstance ??= new ReflectionClass(static::class);
 	}
@@ -229,7 +226,7 @@ trait MarshalTrait {
 		return self::$cachedHolders ??= array_map(
 			callback: static fn(ReflectionProperty $property) => new PropertyHolder(
 				property: $property,
-				field: self::getField($property)
+				field: self::getField($property) ?? throw new AssertionError("Field attribute not found on property '{$property->getName()}'")
 			),
 			array: array_filter(
 				array: self::getReflectedInstance()->getProperties(),
@@ -245,7 +242,7 @@ trait MarshalTrait {
 	 * @return Field<mixed, mixed>|null
 	 */
 	private static function getField(ReflectionProperty $property): ?Field {
-		/** @var ReflectionAttribute<Field>|null $attribute */
+		/** @var ReflectionAttribute<Field<mixed, mixed>>|null $attribute */
 		$attribute = $property->getAttributes(Field::class)[0] ?? null;
 		return match (true) {
 			$attribute instanceof ReflectionAttribute => $attribute->newInstance(),
@@ -256,7 +253,7 @@ trait MarshalTrait {
 	/**
 	 * This method is used as a way to check if a property's types are compatible with the value being set
 	 *
-	 * @throws GeneralMarshalException
+	 * @throws UnmarshalException
 	 */
 	private static function checkType(ReflectionProperty $property, mixed $value): void {
 		$type = $property->getType();
@@ -265,15 +262,16 @@ trait MarshalTrait {
 		}
 		$valueTypeName = get_debug_type($value);
 		if ($type instanceof ReflectionNamedType && $valueTypeName !== $type->getName() && !self::hasEdgeCase($type, $value)) {
-			throw new GeneralMarshalException("Field '{$property->getName()}' must be of type '{$type->getName()}', got '$valueTypeName'");
+			throw new UnmarshalException("Field '{$property->getName()}' must be of type '{$type->getName()}', got '$valueTypeName'");
 		} else if ($type instanceof ReflectionUnionType && !in_array($valueTypeName, ($types = self::getTypeNames($type)), true) && !self::hasEdgeCase($type, $value)) {
 			$imploded = implode(separator: ", ", array: $types);
-			throw new GeneralMarshalException("Field '{$property->getName()}' must be one of the types ($imploded), got '$valueTypeName'");
+			throw new UnmarshalException("Field '{$property->getName()}' must be one of the types ($imploded), got '$valueTypeName'");
 		}
 	}
 
 	/**
 	 * This method is used to compile a list of names associated with a given type
+	 * @return array<string>
 	 */
 	private static function getTypeNames(ReflectionType $type): array {
 		return array_map(
--- src/libMarshal/exception/MarshalException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace libMarshal\exception;
-
-class MarshalException extends GeneralMarshalException { }
