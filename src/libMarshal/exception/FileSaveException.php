fae80f80d72c35b8a735057d792d601e8a46a13e
Em Jordan <sylvrs@proton.me>
2023-07-09 00:07:50 -0500

    chore: add exception class for file saving

---
+++ src/libMarshal/exception/FileSaveException.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace libMarshal\exception;
+
+use RuntimeException;
+
+/**
+ * FileSaveException is the exception thrown when an error occurs during saving a file.
+ */
+class FileSaveException extends RuntimeException {
+}
