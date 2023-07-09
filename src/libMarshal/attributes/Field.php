7b3144d945de9623b491805c2f8ebf67ec6f0d50
Matthew Jordan <mattjordanbusiness@gmail.com>
2023-07-09 00:03:00 -0500

    feat!(field): implement `resolveEnumByName` & allow for type of parser to be `Parseable`

--- src/libMarshal/attributes/Field.php
+++ src/libMarshal/attributes/Field.php
@@ -19,14 +19,20 @@ class Field {
 
 	/**
 	 * @param string $name - This is the name of the field when marshaling/unmarshaling.
-	 * @param class-string<Parseable<TSerialized, TParsed>>|null $parser - This is the class that will be used to parse & serialize the value.
+	 * @param Parseable<TSerialized, TParsed>|class-string<Parseable<TSerialized, TParsed>>|null $parser - This is the class that will be used to parse & serialize the value.
 	 * @param bool $allowUninitialized - If set to true, the field will not be required to be initialized when unmarshaling.
+	 * @param bool $resolveEnumByName - If set to true, backed enums will be unmarshaled by name instead of by value.
 	 */
 	public function __construct(
 		public readonly string $name = "",
-		?string $parser = null,
-		public readonly bool $allowUninitialized = false,
+		Parseable|string|null  $parser = null,
+		public readonly bool   $allowUninitialized = false,
+		public readonly bool   $resolveEnumByName = false,
 	) {
-		$this->parser = is_string($parser) ? new $parser() : null;
+		$this->parser = match (true) {
+			$parser instanceof Parseable => $parser,
+			is_string($parser) => new $parser(),
+			default => null,
+		};
 	}
 }
